a8ee5995b5bcf861630cb257e8ee204778b65f93
Debo <85853814+olugbadebo@users.noreply.github.com>
2023-04-28 02:21:48 +0000

    Update 3 files

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import "./ResetButton.css"
-export const ResetButton = ({resetBoard}) => {
-    return(
-        <button className="reset-btn" onClick = {resetBoard}>Reset</button>
-    )
-}
+import React from "react";
+import "./ResetButton.css";
+export const ResetButton = ({ resetBoard, resetGame }) => {
+  return (
+    <div class="btn-group">
+      <button className="reset-btn" onClick={resetBoard}>
+        Reset
+      </button>
+      <button className="reset-btn" onClick={resetGame}>
+        New game
+      </button>
+    </div>
+  );
+};
 //  const ResetGame = ({}) => {
 //      return(
 //          <button className="resetGame-btn" onClick = {ResetGame}>ResetGame</button>
